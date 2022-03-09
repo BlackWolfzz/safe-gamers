6718dcfe5f1d2d295e23053a06a785d7dbe7f63f
Black Wolf <nimeshfernandz6@gmail.com>
2022-03-09 16:44:30 +0530

    update4

--- Javascript/polls.js
+++ Javascript/polls.js
@@ -1,4 +1,4 @@
-let poll = {
+/* let poll = {
     quiz: "What is your favorite Platform?",
     answers: [
         "PC", "Playstation", "XBox", "Nintendo", "Mobile"
@@ -13,4 +13,46 @@ let pollDOM = {
     answers: document.querySelector(".poll .answers")
 };
 
-pollDOM.quiz.innerText = poll.quiz;
+pollDOM.quiz.innerText = poll.quiz; 
+
+let tabHeader = document.getElementsByClassName("tab-header")[0];
+let tabIndiactor = document.getElementsByClassName("tab-indicator")[0];
+let tabBody = document.getElementsByClassName("tab-body")[0];
+
+let tabsPane = tabHeader.getElementsByTagName("div");
+
+for(let i=0; i<tabsPane.length;i++){
+    tabsPane[i].addEventListener("click", function(){
+        tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+        tabsPane[i].classList.add("active");
+        tabBody.getElementsByClassName("active")[0].classList.remove("active");
+        tabBody.getElementsByTagName("div").classList.add("active");
+
+        tabIndiactor.style.left = 'calc(calc(100% / 2) * ${i})'
+    });
+} */
+
+const tabs = document.querySelectorAll(".tabs li");
+const contents = document.querySelectorAll(".contents");
+
+tabs.forEach((tab, index) =>{
+    tab.addEventListener("click", () =>{
+
+        // To remove active class from previous tab
+        tabs.forEach((tab) => tab.classList.remove("active"));
+
+        tab.classList.add("active");
+
+        // To Show content ccording to the selected tab
+
+        // To hide previous tab content
+        contents.forEach((c) => c.classList.remove("active"));
+
+        contents[index].classList.add("active");
+    });
+});
+
+
+
+// To run the animation intially when the page loads
+tabs[0].click();
