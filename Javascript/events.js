92bdf148a99457ad1c2e501b2a62121349bec3ae
Black Wolf <nimeshfernandz6@gmail.com>
2022-03-16 07:38:23 +0530

    16-03-2022_7.37am_events_mail_done

--- Javascript/events.js
+++ Javascript/events.js
@@ -1,16 +1,16 @@
 function sendMail(params) {
     var tempParams = {
-        from_name: document.getElementById("name").value,
-        to_name: document.getElementById("email").value,
-        message: document.getElementById("title").value,
+        from_name: document.getElementById("fromname").value,
+        to_name: document.getElementById("toemail").value,
+        message: document.getElementById("thetitle").value,
         message_1: document.getElementById("date").value,
         message_2: document.getElementById("appt").value,
         message_3: document.getElementById("e-type").value,
         message_4: document.getElementById("url").value,
     };
 
-    emailjs.send('service_73etxk7', 'template_i7gqjmq', tempParams)
+    emailjs.send('service_qffsm07', 'template_i7gqjmq', tempParams)
     .then(function(res) {
-        console.log("Success", res.status);
+        console.log("success", res.status);
     })
 }
